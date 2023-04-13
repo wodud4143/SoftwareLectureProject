52acddb553da7acf466cddd21ef825b751369104
wkdgydms9121 <84010233+wkdgydms9121@users.noreply.github.com>
2023-04-13 21:28:11 +0900

    MoneyPutOut

--- Virture_stock_trading/src/main/java/com/mycompany/layout/mypage.java
+++ Virture_stock_trading/src/main/java/com/mycompany/layout/mypage.java
@@ -51,6 +51,11 @@ public class mypage extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("굴림", 0, 18)); // NOI18N
         jButton1.setText("입금하기");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setFont(new java.awt.Font("굴림", 0, 18)); // NOI18N
         jButton2.setText("출금하기");
@@ -138,6 +143,10 @@ public class mypage extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
