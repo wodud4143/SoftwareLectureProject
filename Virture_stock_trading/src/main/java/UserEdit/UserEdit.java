5c6c33d5679256853a5f897c38107fcd47c2f50a
wodud4143 <wodud4143@naver.com>
2023-04-13 21:11:19 +0900

    테스트

--- Virture_stock_trading/src/main/java/UserEdit/UserEdit.java
+++ Virture_stock_trading/src/main/java/UserEdit/UserEdit.java
@@ -1,61 +1,30 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- 테스트xptmxm123123*/
-package com.mycompany.MvcPattern;
-
-/**
- *
- * @author ijaeyeong
+ * Clllllick nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package UserEdit;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import com.mycompany.SingletonPattern.User;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-
-import com.google.gson.Gson;
-import com.mycompany.SingletonPattern.User;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class SignUpController {
-    private SignUp view;
-    private Gson gson;
+/**
+ *
+ * @author ijaeyeong
+ */
+public class UserEdit {
 
-    public SignUpController(SignUp view) {
-        this.view = view;
-        this.gson = new Gson();
-
-        this.view.getSubmitButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                registerUser();
-            }
-        });
-    }
-
-    private void registerUser() {
-        String username = view.getUsernameField().getText();
-        String userId = view.getUserIdField().getText();
-        String password = new String(view.getPasswordField().getPassword());
-
-        User user = User.getInstance();
-        user.setPassword(password);
-        user.setName(username);
-        user.setId(userId);
-        saveUserToFile(user);
-    }
-
-    private void saveUserToFile(User user) {
-        if(SignUp.doubleCheck > 0){
-            try {
+    UserEdit() {
+        try {
+            User user = User.getInstance();
             JSONArray users;
 
             // 파일에서 기존 데이터 읽어오기
@@ -76,12 +45,9 @@ public class SignUpController {
 
             // 파일에 데이터 쓰기
             Files.write(Paths.get("users.json"), (users.toJSONString() + System.lineSeparator()).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-            }
-            catch (IOException | ParseException ex) {
+        } catch (IOException | ParseException ex) {
             ex.printStackTrace();
-            }
         }
-        
     }
 
 }
