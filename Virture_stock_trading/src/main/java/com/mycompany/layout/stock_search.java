7b2967c6dac20dc240c41fb15188f9e01c1fc499
YunChangDongEuiUni <129272192+YunChangDongEuiUni@users.noreply.github.com>
2023-05-15 02:05:38 +0900

    test commit
    test comit

--- Virture_stock_trading/src/main/java/com/mycompany/layout/stock_search.java
+++ Virture_stock_trading/src/main/java/com/mycompany/layout/stock_search.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package com.mycompany.layout;
-
+//test
 import com.mycompany.Others.Rate;
 import com.mycompany.Stocksearch.GoogleFinanceCrawler;
 import java.io.IOException;
